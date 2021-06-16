5949c2ea5da607b0933f58dde1400a81478c358b
nahyeon-an <skgus923@gmail.com>
2021-06-16 21:36:01 +0900

    update flask

---
+++ flask/flask_blog/flaskblog.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+# url_for 함수 : static 의 위치를 찾아줌
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    # posts에 전달하는 인자 이름(빨간색 글자)으로 템플릿에서 데이터에 접근
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
